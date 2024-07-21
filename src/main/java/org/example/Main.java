a711acfbf19242816843c000579842ba11e89996
supreeya <poteii.pasel@icloud.com>
2024-07-21 21:59:11 +0700

    add class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -13,5 +13,19 @@ public class Main {
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
         }
+
+        Triangle triangleA = new Triangle(15, 8, 15,
+                8, 17);
+        Triangle triangleB = new Triangle(3, 2.598,
+                3, 3, 3);
+        double triangleAArea = triangleA.findArea();
+        System.out.println(triangleAArea);
+        System.out.println(triangleA.sideLenThree);
+
+        double triangleBArea = triangleB.findArea();
+        System.out.println(triangleBArea);
+        System.out.println(triangleB.base);
+
+        System.out.println(Triangle.numOfSides);
     }
 }
