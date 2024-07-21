b3f4da33718627d2b16a7cd44253a0de9f60a906
supreeya <poteii.pasel@icloud.com>
2024-07-21 20:46:32 +0700

    add data type

--- src/main/java/org/example/HelloWorld.java
+++ src/main/java/org/example/HelloWorld.java
@@ -1,4 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class HelloWorld {
+
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double gpa = 3.45;
+        boolean attendence = true;
+        String firstname = "Poteii";
+        String lastname = "De Aon";
+        //charAt = grab first char
+        char stdFirstInit = firstname.charAt(0);
+        char stdLastInit = lastname.charAt(0);
+
+        System.out.println(studentAge);
+        System.out.println(gpa);
+        System.out.println(stdFirstInit);
+        System.out.println(stdLastInit);
+        System.out.println(attendence);
+        System.out.println(firstname);
+        System.out.println(lastname);
+
+        System.out.println(firstname + " " + lastname + " has gpa of " + gpa);
+        System.out.println("What you want to update it to?");
+        Scanner input = new Scanner(System.in);
+        gpa = input.nextDouble();
+        System.out.println(firstname + " " + lastname + " now has gpa of " + gpa);
+
+    }
 }
